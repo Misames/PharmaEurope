eb53a14339606e903bd2b27aa716a6ac10c3ebaf
Misames <francois.bouscaillou@lycee-saint-john-perse.fr>
2020-03-17 14:49:31 +0100

    add jdom & Junittest

--- src/pharmaeurope/PharmaEurope.java
+++ src/pharmaeurope/PharmaEurope.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pharmaeurope;
 
 /**
  *
- * @author François
+ * @author François Bouscaillou
  */
 public class PharmaEurope {
 
@@ -15,8 +10,6 @@ public class PharmaEurope {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hello world");
     }
-    
 }
