69109041c4c1f57ac334d10975accdeab275cf10
Misames <francois.bouscaillou@lycee-saint-john-perse.fr>
2020-03-29 13:11:26 +0200

    add SecteurDAO

---
+++ src/DAO/SecteurDAO.java
@@ -0,0 +1,41 @@
+package DAO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import LesClasses.Secteur;
+
+/**
+ * SecteurDAO
+ */
+public class SecteurDAO {
+    private Connection connexion = null;
+    private Statement stmt = null;
+    private ResultSet rs = null;
+
+    public SecteurDAO() {
+        try {
+            connexion = Connect.getConnexion();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public ArrayList<Secteur> getLesSecteur() throws SQLException {
+        ArrayList<Secteur> res = new ArrayList<Secteur>();
+
+        String sql = "SELECT * FROM secteur";
+        stmt = connexion.createStatement();
+        rs = stmt.executeQuery(sql);
+
+        while (rs.next()) {
+            String secCode = rs.getString("SEC_CODE");
+            String secLibelle = rs.getString("SEC_LIBELLE");
+            Secteur unSecteur = new Secteur(secCode, secLibelle);
+            res.add(unSecteur);
+        }
+        return res;
+    }
+
+
+}
